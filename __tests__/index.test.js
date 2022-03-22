754829d54c05e571141998fc1b3a94ee2a412aee
Atchuth <Karri.Atchuta.Rama.Reddy@cevalogistics.com>
2022-03-22 20:52:58 +0530

    Added Test cases and implemented check result code for tasks and depedency

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,3 +1,25 @@
-test('adds 1 + 2 to equal 3', () => {
-    expect(3).toBe(3);
-  });
+const checkResult = require('../index');
+
+//Test case for No Tasks and dependecy
+test('No Tasks and dependecy', () => {
+    var taskData = [] ,dependenciesData = [];
+    expect(checkResult(taskData, dependenciesData)).toEqual([])
+});
+
+//Test case for Only Tasks and no dependecy
+test('Only Tasks and no dependecy', () => {
+    var taskData = ['a','b'] ,dependenciesData = [];
+    expect(checkResult(taskData, dependenciesData)).toEqual(['a','b'])
+});
+
+//Test case for Having Tasks and  dependecy 
+test('Having Tasks and  dependnecy', () => {
+    var taskData = ['a','b','c'] ,dependenciesData = ['a:b','b:c'];
+    expect(checkResult(taskData, dependenciesData)).toEqual(['c','b','a'])
+});
+
+//Test case for Having Tasks and  dependnecy Example2
+test('Having Tasks and  dependecy Example2', () => {
+    var taskData = ["a", "b", "c","d","e","f"] ,dependenciesData = ["a:b","a:c","b:d", "c:d","e:f"];
+    expect(checkResult(taskData, dependenciesData)).toEqual([ 'd', 'b', 'c', 'a', 'f', 'e' ])
+});
--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log('First code');
+//Fetch Results function
+const checkResult = (taskData, dependenciesData) => {
+  var tasks = taskData,
+    dependencies = dependenciesData,
+    result = [];
+  // Checking for dependencies are there or not
+  if (dependencies.length !== 0) {
+    //Todo for cyclic dependency
+    if (!true) {
+      return "Cyclic dependency";
+    }
+    //Logic for having dependency
+    else {
+      result = tasks;
+      dependencies.map((item) => {
+        var dependentItem = item.split(":");
+        var independentIndex = result.indexOf(dependentItem[0]);
+        var dependentIndex = result.indexOf(dependentItem[1]);
+        if (dependentIndex > independentIndex) {
+          result.splice(dependentIndex, 1);
+          //p1 and p2 are slices or resultant array
+          var p1 = result.slice(0, independentIndex);
+          var p2 = result.slice(independentIndex);
+          p1.push(dependentItem[1]);
+          p1 = p1.concat(p2);
+          result = p1;
+        }
+      });
+      return result;
+    }
+  } else {
+    result = tasks;
+    return result;
+  }
+};
+
+//Pass taskData and dependenciesData
+var taskData = ["a", "b", "c", "d", "e", "f"],
+  dependenciesData = ["a:b", "a:c", "b:d", "c:d", "e:f"];
+
+//Calling result function and passing taskData and dependenciesData as params to function
+checkResult(taskData, dependenciesData);
+
+module.exports = checkResult;
